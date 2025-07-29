800e676d78ffb9f82436fdc136a9592bcb612652
LFelipedev <luiz.fma.up@gmail.com>
2025-07-29 11:09:00 -0300

    refactor button and add icon on props

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,12 +1,22 @@
-function Button ({text, variant = 'primary', onClick}){
-  return(
-    <button className={`btn-true p-1.5 rounded-lg bg-gray-50 cursor-pointer  
-      ${variant === 'primary'
-        ? 'bg-rose-950 text-white border border-rose-950' : 'bg-white text-rose-950 border border-rose-950'
-       }`} onClick={onClick}>
-      {text}
+import React from "react";
+
+function Button({ icon, text, variant = 'primary', onClick }) {
+  let variantClass = "";
+
+  if (variant === "primary") {
+    variantClass = "bg-[var(--vinho-primario)] text-white border border-[var(--vinho-primario)]";
+  } else {
+    variantClass = "bg-white text-[var(--vinho-primario)] border border-[var(--vinho-primario)]";
+  }
+
+  return (
+    <button
+      className={`btn-true p-1.5 rounded-lg bg-gray-50 cursor-pointer ${variantClass}`}
+      onClick={onClick}
+    >
+      {icon} {text}
     </button>
-  )
+  );
 }
 
 export default Button;
