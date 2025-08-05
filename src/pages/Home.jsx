57baa763e886f8eb411b58bf53e2f29fdd269f02
LFelipedev <luiz.fma.up@gmail.com>
2025-08-04 21:11:08 -0300

    corrigindo o problema da home não aparecer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,19 @@
+import React from "react";
 import Button from "../components/ui/Button";
 import Sidebar from "../components/layout/Sidebar";
 
+function Home() {
 
-function Home(){
-    <>
-        <div className="flex">
-            <Sidebar/>
-            <div className="p-4 flex flex-col gap-4">
-               <h1 className="text-xl font-bold">Home</h1>
-
-                {/* Botões visíveis */}
-                <Button variant="secondary" text="Voltar" />
-                <Button text="Avançar" />
+    return (
+        <>
+            <div className="flex">
+                <Sidebar />
+                <div className="p-4 flex flex-col gap-4">
+                    <h1 className="text-xl font-bold">Home</h1>
+                </div>
             </div>
-        </div>
-    </>
+        </>
+    );
+
 }
 export default Home;
